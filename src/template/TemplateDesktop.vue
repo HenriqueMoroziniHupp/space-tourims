a90250ce930e99862fd9c3f9875361b4aac2230d
henriquehupp <henriquehupp@penguin>
2022-05-19 16:04:15 -0300

    init DestinationContent

---
+++ src/template/TemplateDesktop.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="template">
+    <slot></slot>
+    <!-- Conteudo aqui-->
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.template {
+  display: grid;
+  place-items: center;
+  // display: grid;
+  // max-width: 1050px;
+  // margin: 0 auto;
+}
+</style>
--- src/views/ViewDestination.vue
+++ src/views/ViewDestination.vue
@@ -3,18 +3,15 @@ import { RouterView } from 'vue-router'
 import DestinationPresentation from '@/components/content/contentDestination/DestinationPresentation.vue'
 // import DestinationNav from '../components/content/contentDestination/DestinationNav.vue'
 import Moon from '../components/content/contentDestination/destinations/Moon.vue'
+import TemplateDesktop from '../template/TemplateDesktop.vue'
 </script>
 
 <template>
   <div>
     <!-- <DestinationNav /> -->
-    <RouterView />
-    <!-- <DestinationPresentation
-      headerTitle="MOON"
-      description="See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites."
-      distance="384,400 KM"
-      time="3 DAYS"
-    /> -->
+    <TemplateDesktop>
+      <RouterView />
+    </TemplateDesktop>
   </div>
 </template>
 
